7e185935ed269543d677664b5e62b1171d70ad39
jittel <nathan.c.ginn@gmail.com>
2022-06-09 18:05:38 -0700

    added reaction schema and reaction controller stuff

--- models/Thought.js
+++ models/Thought.js
@@ -1,19 +1,51 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create Thought model
-const thoughtSchema = new Schema(
+const reactionSchema = new Schema(
     {
-        thoughtText: [{
+        reactionId: [{
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        }],
+        reactionBody: [{
             type: String,
-            required: true
+            required: true,
+            maxlength:280,
         }],
         createdAt: [{
             type: Date,
             default: Date.now,
         }],
         username: [{
-            type
+            type: String,
+            required:true,
         }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: [{
+            type: String,
+            required: true,
+            maxlength:280,
+        }],
+        createdAt: [{
+            type: Date,
+            default: Date.now,
+        }],
+        username: [{
+            type: String,
+            required:true,
+        }],
+        reactions: [reactionSchema]
     },
     {
         toJSON: {
